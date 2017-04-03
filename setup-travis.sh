0c42f1c04424ad62ca45756b43c5ee000f167452
Jean-Rémy Buchs <jrb0001@692b8c32.de>
2017-04-03 20:39:18 +0200

    configure Travis CI

---
+++ setup-travis.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Setting up submodules (HTTPS)"
+sed -i 's|git@github.com:|https://github.com/|g' .gitmodules
+git submodule update --init
+git submodule foreach sed -i 's|git@github.com:|https://github.com/|g' .gitmodules
+git submodule update --init --recursive && ./rebuild.sh
