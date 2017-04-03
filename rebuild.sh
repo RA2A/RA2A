14256ff7a30526962662ad02c52fc27f87ec92f5
Jean-Rémy Buchs <jrb0001@692b8c32.de>
2017-04-03 22:47:49 +0200

    configure Travis CI

--- rebuild.sh
+++ rebuild.sh
@@ -7,5 +7,5 @@ fail() {
 
 echo "Compiling engine"
 cd engine
-make clean all-dependencies all || fail "please run $0 again"
+make clean all-dependencies all SDK="-sdk:4.5" || fail "please run $0 again"
 cd ..
