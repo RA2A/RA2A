3270fc7ecf515e84232ab070e1a3e4e4f288238c
Jean-Rémy Buchs <jrb0001@692b8c32.de>
2017-04-05 20:35:56 +0200

    moved submodules

--- rebuild.sh
+++ rebuild.sh
@@ -9,3 +9,9 @@ echo "Compiling engine"
 cd engine
 make clean all-dependencies all SDK="-sdk:4.5" || fail "please run $0 again"
 cd ..
+
+echo "Compiling AS"
+cd mods/as && make clean mod && cd ../..
+
+echo "Compiling RA2A"
+cd mods/ra2a && make clean mod && cd ../..
